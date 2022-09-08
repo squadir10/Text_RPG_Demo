c4a48e4672ea8737ff6dc11fd0b67909eaa19eca
Sabrina Quadir <97366704+squadir10@users.noreply.github.com>
2022-09-08 17:45:47 -0400

    The beginning...

---
+++ Text_RPG_Demo/Program.cs
@@ -0,0 +1,6 @@
+﻿// See https://aka.ms/new-console-template for more information
+// Author: Sabrina Quadir
+// Purpose: Seeing if I can make a text adventure game in preparation for the final project
+//which will be a combination of C# and Unity 
+Console.WriteLine("Hello, World!");
+Console.WriteLine("This is the start of the game");
