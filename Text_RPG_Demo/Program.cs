117d691a97d6f29596a9025970f0e53f32b06a2e
Sabrina Quadir <97366704+squadir10@users.noreply.github.com>
2022-10-01 20:32:34 -0400

    holy shit that's so cool (text animation test)

--- Text_RPG_Demo/Program.cs
+++ Text_RPG_Demo/Program.cs
@@ -2,5 +2,29 @@
 // Author: Sabrina Quadir
 // Purpose: Seeing if I can make a text adventure game in preparation for the final project
 //which will be a combination of C# and Unity 
-Console.WriteLine("Hello, World!");
-Console.WriteLine("This is the start of the game");
+
+
+namespace RPG_Game {
+    internal class program {
+        public static void print(string text, int text_speed = 40) {
+
+            foreach (char c in text) {
+                Console.Write(c);
+                System.Threading.Thread.Sleep(text_speed);
+            }
+            Console.WriteLine();
+        }
+        
+        public static void Main(string[] args) {
+            
+            Console.Title = "Window";
+            print("Hello... there", 100);
+            print("This is a test to see if the animation works");
+            print("bruh idk I hope this works", 75);
+
+
+        }
+        
+    }
+   
+}
